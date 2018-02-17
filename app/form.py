fbcca216524eb646791acaacb26a76d80720265c
KIMARI NELSON <kimari.nelson@mymona.uwi.edu>
2018-02-17 22:58:43 +0000

    Fixed validation

--- app/form.py
+++ app/form.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, flash, session, redirect, url_for
-from wtforms import TextAreaField, StringField
+from wtforms import TextAreaField, StringField,SubmitField
 from wtforms.validators import DataRequired, Email
 from flask_wtf  import FlaskForm
 
@@ -9,9 +9,9 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 
 class MyForm(FlaskForm):
-    name = StringField('name', validators = [DataRequired()])
-    email = StringField('email', validators = [DataRequired(), Email()])
-    subject = StringField('subject', validators = [DataRequired()])
-    message = TextAreaField('message', validators = [DataRequired()])
-
+    name = StringField('name', validators = [DataRequired("Name is required!!!!!!!!!")])
+    email = StringField('email', validators = [DataRequired("Email is required!!!!!!!!!"), Email("Please Enter vaild email name@example.com")])
+    subject = StringField('subject', validators = [DataRequired("Subject for message is required!!!!!!!!!!")])
+    message = TextAreaField('message', validators = [DataRequired("Message is required!!!!!!!!")])
+    button = SubmitField('send')
     
